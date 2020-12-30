4fd7663068f44feedf8fa4a9c909b7e90d828c99
Weizhen Wang <wangwzhen@gmail.com>
2020-12-30 14:50:10 -0600

    add exer 31

---
+++ 31charBool/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char myChar = 'w';
+        char myUnicode = '\u00A9';
+        System.out.println(myUnicode + "" + myChar);
+
+        boolean isOverEighteen = true;
+        System.out.println(isOverEighteen);
+
+
+    }
+}
