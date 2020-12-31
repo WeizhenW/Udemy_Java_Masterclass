2a58df5a11abd4e1bf3ffb0c820de45fcd8fbada
Weizhen Wang <wangwzhen@gmail.com>
2020-12-31 11:11:06 -0600

    add exer 32

--- 32_String/src/com/company/Main.java
+++ 32_String/src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String myString = "You are my sunshine";
+        myString = myString + "!!" + " \u00A92020";
+        System.out.println(myString);
     }
 }
